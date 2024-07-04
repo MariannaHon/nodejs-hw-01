f41596c61190c6f62cb9890765b758c502f0db6a
MariannaHon <imarianna96i@gmail.com>
2024-07-04 21:52:20 +0200

    commit
    after check

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -4,7 +4,8 @@ import { PATH_DB } from '../constants/contacts.js';
 export const getAllContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB, 'utf8');
-        console.log('Вміст файлу:', data);
+        const contacts = JSON.parse(data);
+        return contacts;
     } catch (error) {
         console.error('Error reading contacts', error);
     }
